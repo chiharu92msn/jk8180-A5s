e5d7117df71b7d9240bc4adc77e98eeadbcb5a2e
chiharu92msn <chiharu92@msn.com>
2024-11-09 18:20:56 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,23 +14,22 @@ if  mode == "1":
     pool = loads['pool']
     wallet = loads['wallet']
     password = loads['pass']
+    cpu = loads['cpu']
     print("POOL     =",pool)
     print("WALLET   =",wallet)
     print("PASSWORD =",password)
+    print("CPU      =",cpu)
  POOL=pool
  WALLET=wallet
  PASSWORD=password
-
+ CPU=cpu
  with open("set-miner-off/offline.json", "r", encoding='utf8') as file:
     text = file.read()
     loads = json.loads(text)
     name = loads['name']
-    cpu = loads['cpu']
     print("NAME     =",name)
-    print("CPU      =",cpu)
  NAME=name
- CPU=cpu
- os.system(f"cd ccminer && ./ccminer -a verus -o {POOL} -u {WALLET}.{NAME} -p {PASSWORD} -t {CPU}")
+ os.system(f"cd miner && cd ccminer && ./ccminer -a verus -o {POOL} -u {WALLET}.{NAME} -p {PASSWORD} -t {CPU}")
 
 if  mode == "2":
  with open("set-miner-on/xmrigcc-XFG.json", "r", encoding='utf8') as file:
@@ -40,24 +39,24 @@ if  mode == "2":
     pool = loads['pool']
     wallet = loads['wallet']
     password = loads['pass']
+    cpu = loads['cpu']
     print("ALGO     =",algo)
     print("POOL     =",pool)
     print("WALLET   =",wallet)
     print("PASSWORD =",password)
+    print("CPU      =",cpu)     
  ALGO=algo
  POOL=pool
  WALLET=wallet
- PASSWORD=password
+ PASSWORD=password 
+ CPU=cpu
 
  with open("set-miner-off/offline.json", "r", encoding='utf8') as file:
     text = file.read()
     loads = json.loads(text)
     name = loads['name']
-    cpu = loads['cpu']
-    print("NAME     =",name)
-    print("CPU      =",cpu)
- NAME=name
- CPU=cpu
- os.system(f"cd miner && ./xmrigDaemon -o {POOL} -a {ALGO} -u {WALLET} -p @{NAME} -k, --rig-id= {NAME} -t {CPU}")
+    print("NAME     =",name)    
+ NAME=name 
+ os.system(f"cd miner && cd xmrig &&./xmrig -o {POOL} -a {ALGO} -u {WALLET} -p @{NAME} -k, --rig-id= {NAME} -t {CPU}")
 
 
