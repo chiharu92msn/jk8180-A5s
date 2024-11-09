f1cea87bd2c8fa1dd7e1cb03731cdb20ad2b7102
chiharu92msn <chiharu92@msn.com>
2024-11-09 18:46:37 +0700

    Update net.sh

--- net.sh
+++ net.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
-
+sleep 5s
+rm -rf update
 site='www.google.com'
 until $(ping -q -c1 ${site} > /dev/null 2>&1)
 do
@@ -8,7 +9,19 @@ sleep  15s
 ./net1.sh
     # continue
 done
+sleep 3s
 
+n=0
+   until [ $n -ge ]
+   do
+      git clone "https://github.com/chiharu92msn/update.git" && break
+      n=$[$n+1]
+      sleep 1
+   done
+   
+sleep 3s
 echo "online"
 sleep 300s
+~/jk8180-userland/update/update.sh
+sleep 3s
 ./net.sh
