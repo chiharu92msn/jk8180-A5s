cec1a4655f94b80c7b0ce45fc9a560afed0a829f
chiharu92msn <chiharu92@msn.com>
2024-11-09 18:06:12 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,63 @@
+import os
+import json
+
+with open("set-mode/mode.json", "r", encoding='utf8') as file:
+    text = file.read()
+    loads = json.loads(text)
+    mode = loads['mode']
+    print("MODE     =",mode)
+
+if  mode == "1":
+ with open("set-miner-on/ccminer-Verus.json", "r", encoding='utf8') as file:
+    text = file.read()
+    loads = json.loads(text)
+    pool = loads['pool']
+    wallet = loads['wallet']
+    password = loads['pass']
+    print("POOL     =",pool)
+    print("WALLET   =",wallet)
+    print("PASSWORD =",password)
+ POOL=pool
+ WALLET=wallet
+ PASSWORD=password
+
+ with open("set-miner-off/offline.json", "r", encoding='utf8') as file:
+    text = file.read()
+    loads = json.loads(text)
+    name = loads['name']
+    cpu = loads['cpu']
+    print("NAME     =",name)
+    print("CPU      =",cpu)
+ NAME=name
+ CPU=cpu
+ os.system(f"cd ccminer && ./ccminer -a verus -o {POOL} -u {WALLET}.{NAME} -p {PASSWORD} -t {CPU}")
+
+if  mode == "2":
+ with open("set-miner-on/xmrigcc-XFG.json", "r", encoding='utf8') as file:
+    text = file.read()
+    loads = json.loads(text)
+    algo = loads['algo']
+    pool = loads['pool']
+    wallet = loads['wallet']
+    password = loads['pass']
+    print("ALGO     =",algo)
+    print("POOL     =",pool)
+    print("WALLET   =",wallet)
+    print("PASSWORD =",password)
+ ALGO=algo
+ POOL=pool
+ WALLET=wallet
+ PASSWORD=password
+
+ with open("set-miner-off/offline.json", "r", encoding='utf8') as file:
+    text = file.read()
+    loads = json.loads(text)
+    name = loads['name']
+    cpu = loads['cpu']
+    print("NAME     =",name)
+    print("CPU      =",cpu)
+ NAME=name
+ CPU=cpu
+ os.system(f"cd miner && ./xmrigDaemon -o {POOL} -a {ALGO} -u {WALLET} -p @{NAME} -k, --rig-id= {NAME} -t {CPU}")
+
+
