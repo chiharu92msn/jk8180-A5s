57a155d9636859a810a46da3007d3e8e6fa64c29
chiharu92msn <chiharu92@msn.com>
2024-11-09 20:00:47 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 rm -rf set-miner-on
+rm -rf set-mode
 
 site='www.github.com'
 until $(ping -q -c1 ${site} > /dev/null 2>&1)
@@ -15,6 +16,17 @@ n=0
    until [ $n -ge ]
    do
       git clone "https://github.com/chiharu92msn/set-miner-on.git" && break
+      n=$[$n+1]
+      sleep 1
+   done
+   
+sleep 3s
+sleep 3s
+
+n=0
+   until [ $n -ge ]
+   do
+      git clone "https://github.com/chiharu92msn/set-mode.git" && break
       n=$[$n+1]
       sleep 1
    done
