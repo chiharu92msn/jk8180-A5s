e20500c7c051838676465ca5def051ebcb951780
chiharu92msn <chiharu92@msn.com>
2024-11-10 23:14:11 +0700

    Update net3.sh

--- net3.sh
+++ net3.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
-site='www.google.com'
-until $(ping -q -c1 ${site} > /dev/null 2>&1)
-do
-    echo "${site} is unreachable. Retrying(3)"
-sleep  15s
-killall com.termux
+rm -rf update
+n=0
+   until [ $n -ge ]
+   do
+      git clone "https://github.com/chiharu92msn/update.git" && break
+      n=$[$n+1]
+sleep 3s
+echo "www.google.com is unreachable. Retrying(3)"
+sleep 12s
+pkill -15 -u $(id -un)
     # continue
 done
 
