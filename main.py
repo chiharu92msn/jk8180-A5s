f0a2082a308a243111ceb565dc15cdd94aba6673
chiharu92msn <chiharu92@msn.com>
2024-11-11 21:53:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -59,4 +59,32 @@ if  mode == "2":
  NAME=name 
  os.system(f"cd miner && cd xmrig && cd build &&./xmrig -o {POOL} -a {ALGO} -u {WALLET} -p @{NAME} -k, --rig-id= {NAME} -t {CPU}")
 
+if  mode == "3":
+ with open("set-miner-on/xmrig-WTIP+Wrkz.json", "r", encoding='utf8') as file:
+    text = file.read()
+    loads = json.loads(text)
+    algo = loads['algo']
+    pool = loads['pool']
+    wallet = loads['wallet']
+    password = loads['pass']
+    cpu = loads['cpu']
+    print("ALGO     =",algo)
+    print("POOL     =",pool)
+    print("WALLET   =",wallet)
+    print("PASSWORD =",password)
+    print("CPU      =",cpu)     
+ ALGO=algo
+ POOL=pool
+ WALLET=wallet
+ PASSWORD=password 
+ CPU=cpu
+
+ with open("set-miner-off/offline.json", "r", encoding='utf8') as file:
+    text = file.read()
+    loads = json.loads(text)
+    name = loads['name']
+    print("NAME     =",name)    
+ NAME=name 
+ os.system(f"cd miner && cd xmrig && cd build &&./xmrig -o {POOL} -a {ALGO} -u {WALLET} -p "{PASSWORD}" -k, --rig-id= {NAME} -t {CPU}")
+
 
